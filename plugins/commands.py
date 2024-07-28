3f4130910cadd453fc2ef259174236ccd2fb2cd2
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2024-07-28 14:04:39 +0530

    Add files via upload

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
 import os
 import logging
@@ -24,9 +24,9 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
 
 def get_size(size):
@@ -40,17 +40,17 @@ def get_size(size):
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
 async def delete_after_delay(message: Message, delay):
     await asyncio.sleep(AUTO_DELETE_TIME)
     await message.delete()
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ0
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
 
 @Client.on_message(filters.command("start") & filters.incoming)
@@ -60,10 +60,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@YADUVANSHI_XD')
             ],[
-            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
-            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_botz')
+            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/YADUVANSHIXSUPPORT'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/YADUVANSHIXBOTZ')
             ],[
             InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', callback_data='clone')
             ],[
@@ -79,9 +79,10 @@ async def start(client, message):
         )
         return
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
+
     
     data = message.command[1]
     try:
@@ -142,10 +143,11 @@ async def start(client, message):
             await asyncio.sleep(1) 
         await sts.delete()
         return
+        
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
     
     elif data.split("-", 1)[0] == "DSTORE":
         sts = await message.reply("**🔺 ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**")
@@ -198,9 +200,10 @@ async def start(client, message):
             await asyncio.sleep(1) 
         return await sts.delete()
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
+
 
     files_ = await get_file_details(file_id)           
     if not files_:
@@ -213,7 +216,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = '@VJ_Botz  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = '@real_moviesAdda4  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
@@ -245,9 +248,10 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
+
     
     files = files_[0]
     title = files.file_name
@@ -268,9 +272,9 @@ async def start(client, message):
         protect_content=True if pre == 'filep' else False,
         )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
 @Client.on_message(filters.command('api') & filters.private)
 async def shortener_api_handler(client, m: Message):
@@ -287,9 +291,9 @@ async def shortener_api_handler(client, m: Message):
         await update_user_info(user_id, {"shortener_api": api})
         await m.reply("<b>Shortener API updated successfully to</b> " + api)
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
 @Client.on_message(filters.command("base_site") & filters.private)
 async def base_site_handler(client, m: Message):
@@ -306,9 +310,10 @@ async def base_site_handler(client, m: Message):
         await update_user_info(user_id, {"base_site": base_site})
         await m.reply("<b>Base Site updated successfully</b>")
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
+
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
@@ -332,16 +337,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
     
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+            InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ᴍʏ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Yaduvanshi_XD')
             ],[
-            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
-            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_botz')
+            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/YaduvanshiXsupport'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/Yaduvanshixbotz')
             ],[
             InlineKeyboardButton('🤖 ᴄʀᴇᴀᴛᴇ ʏᴏᴜʀ ᴏᴡɴ ᴄʟᴏɴᴇ ʙᴏᴛ', callback_data='clone')
             ],[
@@ -362,9 +367,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
     
     elif query.data == "clone":
         buttons = [[
@@ -383,9 +388,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )          
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
     
     elif query.data == "help":
         buttons = [[
@@ -404,9 +409,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )  
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
 
     elif query.data.startswith("generate_stream_link"):
         _, file_id = query.data.split(":")
@@ -426,9 +431,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await asyncio.sleep(1)
             await xo.delete()
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
             
             await log_msg.reply_text(
                 text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
@@ -449,6 +454,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.answer(f"☣something went wrong\n\n{e}", show_alert=True)
             return
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @YADUVANSHIXBOTZ
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@yaduvanshi_xd
+# Ask Doubt on telegram @YaduvanshiXsupport
